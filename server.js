1d32b169acf9b96ac6e2d5013c34a8ef706778fb
Karan sapra <88654249+CraveforSuccess@users.noreply.github.com>
2023-02-12 19:57:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',true);
+
+mongoose.connect("mongodb+srv://Admit-Karan:Iamphenomenol@cluster0.dhlpzib.mongodb.net/ContactInfo");
+
+const app = express();
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+
+
+const Info = {
+    name:{type:String,
+        required:true},
+    Number: {type:Number,
+    required:true,
+    min:10,
+    
+    },
+    Email:String
+};
+
+const Continfo = mongoose.model("Continfo",Info);
+
+const Continfo1 = new Continfo ({
+    name:"karan",
+    Number:"7678126262",
+    Email:"saprakarna001@gmail.com"
+});
+const Continfo2 = new Continfo ({
+    name:"Kismat",
+    Number:"8019290191",
+    Email:"kismat001@gmail.com"
+});
+
+const defaultarr = [Continfo1,Continfo2];
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+app.post('/',(req,res)=>{
+    const Name = req.body.Name;
+    const PhnNum = Number(req.body.PhnNum);
+    console.log(typeof(PhnNum));
+    const Email  = req.body.Email;
+    const NewCont = new Continfo ({
+        name: Name,
+        Number:PhnNum,
+        Email:Email
+    })
+    
+   Continfo.insertMany(NewCont,(err,contact)=>{
+    if(err){
+        console.log(err)
+    }else{
+      console.log(contact);
+      console.log("Success");
+      res.redirect('/');
+    }
+   })
+
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Running server")
+})
